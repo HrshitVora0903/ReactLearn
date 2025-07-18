4fd29a320decb6fd96157665f29f08a65d7f3dbe
hrsh <hrshpatel25@gmail.com>
2025-07-18 18:14:03 +0530

    22 Mapping components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,27 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar 
-        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4LtCrYlmpYUw8cSAmmiC69oRYG1U_qdY_YA&s"
-        />
-      <Card 
+      {contacts.map(createCard)}
+      {/* <Card 
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -27,7 +40,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
