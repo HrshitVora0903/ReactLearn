0759d60d307b995678d05d79f0d832c1fa4276cf
hrsh <hrshpatel25@gmail.com>
2025-07-23 14:26:42 +0530

    33 JS ES6 destructuring

--- src/index.js
+++ src/index.js
@@ -1,28 +1,51 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import cars from "./practice";
+// import { use } from "react";
+// import animals, { useAnimal } from "./data";
+
+//DESTRUCTURE ARRAYS
+// const [cat,dog] = animals;
+ 
+// const [animal, makeSound] = useAnimal(cat);
+// console.log(animal);
+// makeSound();
+// //console.log(cat);
+// const {sound,name} = cat;
+// console.log(name);
+
+
+// DESTRUCTRE OBJECTS
+// const {name, sound, feedingRequirements} = cat;
+// console.log(feedingRequirements);
+
+const [honda ,tesla] = cars;
+const {speedStats:{topSpeed: teslaTopSpeed}} = tesla;
+const {speedStats:{topSpeed: hondaTopSpeed}} = honda;
+const {coloursByPopularity: [teslaTopColour, secondcolor]}= tesla;
+const {coloursByPopularity: [hondaTopColour]}= honda;
+
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
-
+root.render( <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Color</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>
 );
 
-
-//Challenge:
-//1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
-
-//2. Given that you can get code to be called every second
-//using the setInterval method.
-//Can you get the time in your <h1> to update every second?
-
-//e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
-// setInterval(sayHi, 1000);
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
