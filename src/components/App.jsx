a74e12283769a9da9248cb5e2e639ce1519d3c83
hrsh <hrshpatel25@gmail.com>
2025-07-25 17:18:43 +0530

    42 practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,50 @@
 import React, { useState } from "react";
-import { preconnect } from "react-dom";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
 
-  const [task, setTask] = useState("");
-  const [items, setItemList] = useState([]);
+  const [items, setItems] = useState([]);
 
 
-  function handleChange(event) {
-    setTask(event.target.value);
-  }
 
-  function handleClick(event) {
-    setItemList(pervItems => {
-      return [...pervItems, task];
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
     });
-    setTask("");
+ 
   }
 
   function deleteItem(id) {
-    setItemList(prevValue => {
-      return prevValue.filter(  
-        (item,index) => {
-          return index !== id;
-        }
-      )
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
     });
-
   }
 
-    return (
-      <div className="container">
-        <div className="heading">
-          <h1>To-Do List</h1>
-        </div>
-        <div className="form">
-          <input type="text"
-            onChange={handleChange}
-            value={task}
-          />
-          <button onClick={handleClick}>
-            <span>Add</span>
-          </button>
-        </div>
-        <div>
-          <ul>
-            {items.map((item, index) =>
-              <ToDoItem
-                key={index}
-                id={index}
-                text={item}
-                onChecked={deleteItem}
-              />
-            )}
-          </ul>
-        </div>
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
       </div>
-    );
-  }
+      <InputArea
+        onAdd={addItem}
+      />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default App;
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
-
-    return (
-        <div onClick= {() => {
-            props.onCheked(props.id);
-        }}>
-            <li>{props.text}</li>
-        </div>);
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
 }
-
 
 export default ToDoItem;
